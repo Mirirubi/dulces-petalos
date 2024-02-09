d7fe6c6477026f2b711e7dabb660eb37835d94bd
Mirirubi <mirirubi6@gmail.com>
2024-02-09 16:46:44 +0100

    Add back button

--- src/app/product/[id]/page.js
+++ src/app/product/[id]/page.js
@@ -1,6 +1,7 @@
 "use client";
 import React, { useState, useEffect } from "react";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Details({ params }) {
   const [data, setData] = useState(null);
@@ -25,25 +26,36 @@ export default function Details({ params }) {
   return (
     <>
       {data ? (
-        <div className="container mx-auto px-4 p-8">
-          <div>{data.name}</div>
-          <div className="product w-60 h-60">
-            <Image
-              src={data.imgUrl}
-              alt={`Imagen de ${data.name}`}
-              unoptimized
-              width={240}
-              height={240}
-              style={{
-                objectFit: "fill",
-              }}
-            ></Image>
+        <div div className="border-2 border-tradewind px-60">
+          <div className="border-2 border-tradewind p-8 flex justify-end">
+            <Link href={`/`}>
+              <div className="rounded-full bg-harp text-center max-w-min px-6 p-1">
+                Back
+              </div>
+            </Link>
           </div>
-          <div>{data.binomialName}</div>
-          <div>{data.price} €</div>
-          <div>{data.wateringsPerWeek} a la semana</div>
-          <div>{data.fertilizerType}</div>
-          <div>{data.heightInCm} cm</div>
+          <div className="container border-2 border-firefly mx-auto px-4 p-8 flex justify-center">
+            <div className="product w-120 h-120">
+              <Image
+                src={data.imgUrl}
+                alt={`Imagen de ${data.name}`}
+                unoptimized
+                width={400}
+                height={400}
+                style={{
+                  objectFit: "fill",
+                }}
+              ></Image>
+            </div>
+            <div className="border-2 border-tradewind p-8">
+              <div>{data.name}</div>
+              <div>{data.binomialName}</div>
+              <div>{data.price} €</div>
+              <div>{data.wateringsPerWeek} a la semana</div>
+              <div>{data.fertilizerType}</div>
+              <div>{data.heightInCm} cm</div>
+            </div>
+          </div>
         </div>
       ) : (
         <p>Cargando. . .</p>
